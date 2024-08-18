9ebe540c366de41a9adbdfadfee1b121724747a1
IhorDid <101325065+IhorDid@users.noreply.github.com>
2024-08-18 21:54:59 +0300

    add

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,19 @@
 import { ContactsCollection } from '../db/models/contacts.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-const getAllContacts = async () => {
-  const contacts = await ContactsCollection.find();
-  return contacts;
+const getAllContacts = async ({ page, perPage }) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+  const contactsQuery = await ContactsCollection.find();
+  const contactsCount = await ContactsCollection.countDocuments().exec();
+  const contacts = await contactsQuery.skip(skip).limit(limit).exec();
+
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
 
 const getContactsById = async (contactId) => {
